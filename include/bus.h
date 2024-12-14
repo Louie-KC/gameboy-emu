6efd9998c6cfe3627d941ce2d3e76d7d1afa5bc2
Louie-KC <83757145+Louie-KC@users.noreply.github.com>
2024-12-14 19:45:49 +1100

    Begin implementation of echo RAM, Object Attr Memory, High AM, and interrupt reg

--- include/bus.h
+++ include/bus.h
@@ -9,11 +9,19 @@
 #define BUS_EXT_RAM_ADDR    0xA000
 #define BUS_WRAM_ADDR       0xC000
 #define BUS_ECHO_ADDR       0xE000
+#define BUS_OAM_ADDR        0xFE00
+#define BUS_UNUSABLE_ADDR   0xFEA0
+#define BUS_IO_REG_ADDR     0xFF00
+#define BUS_HRAM_ADDR       0xFF80
+#define BUS_IE_REG_ADDR     0xFFFF
 
 // Memory sizes
 #define BUS_ROM_BANK_SIZE BUS_ROM_BANK_N_ADDR
 #define BUS_VRAM_SIZE     BUS_WRAM_ADDR - BUS_VRAM_ADDR
 #define BUS_WRAM_SIZE     BUS_ECHO_ADDR - BUS_WRAM_ADDR
+#define BUS_OAM_SIZE      BUS_UNUSABLE_ADDR - BUS_OAM_ADDR
+#define BUS_IO_REG_SIZE   BUS_HRAM_ADDR - BUS_IO_REG_ADDR
+#define BUS_HRAM_SIZE     BUS_IE_REG_ADDR - BUS_HRAM_ADDR
 
 uint8_t bus_read(uint16_t addr);
 void bus_write(uint16_t addr, uint8_t value);
--- src/bus.c
+++ src/bus.c
@@ -3,6 +3,10 @@
 
 uint8_t vram[BUS_VRAM_SIZE];
 uint8_t wram[BUS_WRAM_SIZE];
+uint8_t  oam[BUS_OAM_SIZE];
+uint8_t mmio[BUS_IO_REG_SIZE];
+uint8_t hram[BUS_HRAM_SIZE];
+uint8_t enable_interrupt;
 
 uint8_t bus_read(uint16_t addr) {
     switch (addr) {
@@ -17,8 +21,25 @@ uint8_t bus_read(uint16_t addr) {
         case BUS_WRAM_ADDR ... BUS_ECHO_ADDR - 1:
             // Working RAM
             return wram[addr - BUS_WRAM_ADDR];
+        case BUS_ECHO_ADDR ... BUS_OAM_ADDR - 1:
+            // Echo (of workign RAM) RAM
+            return wram[addr - BUS_ECHO_ADDR];
+        case BUS_OAM_ADDR ... BUS_UNUSABLE_ADDR - 1:
+            // Object Attribute Memory
+            return oam[addr - BUS_OAM_ADDR];
+        case BUS_UNUSABLE_ADDR ... BUS_IO_REG_ADDR - 1:
+            // Not usable memory range
+            return 0;
+        case BUS_IO_REG_ADDR ... BUS_HRAM_ADDR - 1:
+            // Input-Output Registers
+            return mmio[addr - BUS_IO_REG_ADDR];
+        case BUS_HRAM_ADDR ... BUS_IE_REG_ADDR - 1:
+            // High RAM
+            return hram[addr - BUS_HRAM_ADDR];
+        case BUS_IE_REG_ADDR:
+            return enable_interrupt;
         default:
-            // TODO: Echo, OAM, IO registers, HRAM, IE
+            printf("[WARN] bus_read default path taken\n");
             return 0;
     }
 }
@@ -38,8 +59,27 @@ void bus_write(uint16_t addr, uint8_t value) {
             // Working RAM
             wram[addr - BUS_WRAM_ADDR] = value;
             break;
-        default:
-            // TODO: Echo, OAM, IO registers, HRAM, IE
+        case BUS_ECHO_ADDR ... BUS_OAM_ADDR - 1:
+            // Echo (of WRAM) RAM
+            wram[addr - BUS_ECHO_ADDR] = value;
+            break;
+        case BUS_OAM_ADDR ... BUS_UNUSABLE_ADDR - 1:
+            // Object Attribute Memory
+            oam[addr - BUS_OAM_ADDR] = value;
+            break;
+        case BUS_UNUSABLE_ADDR ... BUS_IO_REG_ADDR - 1:
+            // Not usable memory range
+            break;
+        case BUS_IO_REG_ADDR ... BUS_HRAM_ADDR - 1:
+            // Input-Output Registers
+            mmio[addr - BUS_IO_REG_ADDR] = value;
+            break;
+        case BUS_HRAM_ADDR ... BUS_IE_REG_ADDR - 1:
+            // High RAM
+            hram[addr - BUS_HRAM_ADDR] = value;
+            break;
+        case BUS_IE_REG_ADDR:
+            enable_interrupt = value;
             break;
     }
 }
